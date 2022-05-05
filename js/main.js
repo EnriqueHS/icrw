a1acb328e360b14f7d35f3884f6232beee3226d3
Enrique Scapoli <88351578+EnriqueHS@users.noreply.github.com>
2022-05-05 14:22:35 -0300

    mobile

--- js/main.js
+++ js/main.js
@@ -2,16 +2,28 @@ $(document).ready(function () {
 
   $('.hamburger-menu').on('click', function() {
     $('.bar').toggleClass('animate');
-  })
-  
 
-  /* $('.site__menu').on('click keypress', function() {
+  }) 
+  
+/* 
+   $('.site__menu').on('click keypress', function() {
     $(this).find('.bar').toggleClass('animate');
     $('.header').toggleClass('active');
     $('.site__menu > span').text(function(i, text){
       return text === "Menu" ? "Close" : "Menu";
     })
-  }) */
+  })  */
+
+/* 
+
+  let menuActive = $('.header__menu__nav a.active').data('item');
+  $('.header__menu__image img[data-item="'+menuActive+'"]').addClass('js-visible').addClass('active');
+
+  $('.header__menu__nav a').on('mouseenter',function(){
+    let menuItem = $(this).data('item');
+    $('.header__menu__image img[data-item="'+menuItem+'"]').addClass('js-visible').siblings().removeClass('js-visible').removeClass('active');
+  })
+ */
 
 
 });
